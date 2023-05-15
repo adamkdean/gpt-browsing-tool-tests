9e88746a520bd836af18b4e4096c91fa1e5ddfcc
Adam K Dean <adamkdean@googlemail.com>
2023-05-15 14:26:03 +0100

    feat: actually add kv test

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const express = require('express')
 const app = express()
 const port = process.env.HTTP_PORT || 8000
 
+const memory = {}
+
 app.use((req, res, next) => {
+  console.log('--request--')
   console.log('Time:', Date.now())
   console.log('IP:', req.ip)
   console.log('User Agent:', req.headers['user-agent'])
@@ -21,8 +24,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/robots.txt', (req, res) => {
-  // Allow all robots
   res.send('User-agent: *\nAllow: /')
+})
+
+app.get('/get/:key', (req, res) => {
+  const key = req.params.key
+  const value = memory[key]
+  if (value) {
+    res.send(value)
+  } else {
+    res.status(404).json({ error: 'Not found' })
+  }
+})
+
+app.get('/set/:key/:value', (req, res) => {
+  const key = req.params.key
+  const value = req.params.value
+  memory[key] = value
+  res.json({ key, value })
+})
+
+app.get('/keys', (req, res) => {
+  res.json(Object.keys(memory))
 })
 
 app.listen(port, () => console.log(`Listening on ${port}`))
